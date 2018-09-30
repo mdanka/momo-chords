4da57c9d889eb743a598e641d764b0ed1f987d4a
Miklos Danka <danka.miklos@gmail.com>
2018-09-30 13:27:56 +0200

    Refactored chordPrinter; removed ChordParser from export

--- src/chords.ts
+++ src/chords.ts
@@ -1,6 +1,6 @@
 import { IChord } from "./types";
-import { Naming } from "./naming";
 import { ChordParser } from "./chordParser";
+import { ChordPrinter } from "./chordPrinter";
 
 export class Chords {
     public parse(value: string): IChord | undefined {
@@ -12,47 +12,6 @@ export class Chords {
     }
 
     public print(chord: IChord): string {
-        const { rootNote, quality, interval, added, suspended, bassNote } = chord;
-
-        let name: string = "";
-        name += rootNote;
-
-        if (quality !== undefined) {
-            const qualityNames = Naming.qualities.get(quality);
-            if (qualityNames === undefined) {
-                throw new Error(`[Chords] No name found for quality ${quality}`);
-            }
-            name += qualityNames[0];
-        }
-
-        if (interval !== undefined) {
-            const intervalNames = Naming.intervals.get(interval);
-            if (intervalNames === undefined) {
-                throw new Error(`[Chords] No name found for interval ${interval}`);
-            }
-            name += intervalNames[0];
-        }
-
-        if (added !== undefined) {
-            const addedNames = Naming.addeds.get(added);
-            if (addedNames === undefined) {
-                throw new Error(`[Chords] No name found for added ${added}`);
-            }
-            name += addedNames[0];
-        }
-
-        if (suspended !== undefined) {
-            const suspendedNames = Naming.suspendeds.get(suspended);
-            if (suspendedNames === undefined) {
-                throw new Error(`[Chords] No name found for suspended ${suspended}`);
-            }
-            name += suspendedNames[0];
-        }
-
-        if (bassNote !== undefined) {
-            name += "/" + bassNote;
-        }
-
-        return name;
+        return ChordPrinter.print(chord);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./types";
 export * from "./naming";
-export * from "./chordParser";
 export * from "./chords";
