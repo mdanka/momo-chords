80347c4967d7515a6648c6661f53c7f1dabdab31
Miklos Danka <danka.miklos@gmail.com>
2018-09-30 17:34:08 +0200

    Fixed object method references

--- src/chords.ts
+++ src/chords.ts
@@ -3,15 +3,15 @@ import { ChordParser } from "./chordParser";
 import { ChordPrinter } from "./chordPrinter";
 
 export class Chords {
-    public parse(value: string): IChord | undefined {
+    public parse = (value: string): IChord | undefined => {
         return ChordParser.parse(value);
-    }
+    };
 
-    public isChord(value: string): boolean {
+    public isChord = (value: string): boolean => {
         return this.parse(value) !== undefined;
-    }
+    };
 
-    public print(chord: IChord): string {
+    public print = (chord: IChord): string => {
         return ChordPrinter.print(chord);
-    }
+    };
 }
